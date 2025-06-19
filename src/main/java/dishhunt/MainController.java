5fc74c05c0caea008474616a827d4c0109eb357e
HippoThicco <oscarye2126@gmail.com>
2025-06-19 15:21:49 -0400

    created maincontroller to handle logic in ui

---
+++ src/main/java/dishhunt/MainController.java
@@ -0,0 +1,10 @@
+package dishhunt;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+    @FXML
+    private void handleSearchClick() {
+        System.out.println("Search button clicked!");
+    }
+}
