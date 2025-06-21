d78d61b4ce46c268aa6b035dbb1d3a96eb9d1707
HippoThicco <oscarye2126@gmail.com>
2025-06-21 16:34:14 -0400

    created dish class in model to store dish info

---
+++ src/main/java/com/dishhunt/model/Dish.java
@@ -0,0 +1,15 @@
+package com.dishhunt.model;
+
+import java.util.List;
+
+public class Dish {
+	private int id;
+	private String name;
+	private String cuisine;
+	private String description;
+	private List<Recipe> recipes;
+	
+	public void addRecipe(Recipe recipe) {
+		
+	}
+}
