714dd7af8885014a011e0868fa125980b84dc169
HippoThicco <oscarye2126@gmail.com>
2025-06-21 16:06:39 -0400

    updated User class to contain more fields

--- src/main/java/com/dishhunt/model/User.java
+++ src/main/java/com/dishhunt/model/User.java
@@ -1,9 +1,17 @@
 package com.dishhunt.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class User {
+	int id;
 	String username;
 	String password;
 	String name;
 	String bio;
-	
+	String nationality;
+	LocalDate joinDate;
+	List<Recipe> contributions;
+	List<Recipe> favourites;
+	String profilePicturePath;
 }
