b6210707d0cdef3e53f429f287ad03c18f8ead2c
HippoThicco <oscarye2126@gmail.com>
2025-06-21 16:33:46 -0400

    created Recipe class in model package to store recipe info

---
+++ src/main/java/com/dishhunt/model/Recipe.java
@@ -0,0 +1,14 @@
+package com.dishhunt.model;
+
+import java.util.List;
+
+public class Recipe {
+	private int id;
+	private Dish dish;
+	private User author;
+	private String title;
+	private String description;
+	private int calories;
+	private List<Ingredient> ingredients;
+	private String instructions;
+}
