8f47c6ca8559a3997885bc8806b39577fccd113a
HippoThicco <oscarye2126@gmail.com>
2025-06-22 20:41:12 -0400

    created the required placeholder methods in UserDAO

--- src/main/java/com/dishhunt/dao/UserDAO.java
+++ src/main/java/com/dishhunt/dao/UserDAO.java
@@ -1,5 +1,22 @@
 package com.dishhunt.dao;
 
+import com.dishhunt.model.User;
+
 public class UserDAO {
+
+	public User findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean existsbyUsername(String username) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean save(User newUser) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
