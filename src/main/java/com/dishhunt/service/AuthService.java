711cedcc237630cafa4ce69f37b5b7403fec3a5f
HippoThicco <oscarye2126@gmail.com>
2025-06-22 20:38:14 -0400

    completed login and register methods

--- src/main/java/com/dishhunt/service/AuthService.java
+++ src/main/java/com/dishhunt/service/AuthService.java
@@ -1,5 +1,38 @@
 package com.dishhunt.service;
 
-public class AuthService {
+import com.dishhunt.dao.UserDAO;
+import com.dishhunt.model.User;
+import com.dishhunt.util.PasswordUtil;
 
+/**
+ * @param username The entered username.
+ * @param password The entered plaintext password.
+ * @return The authenticated User object if successful; null otherwise;
+ * 
+ */
+public class AuthService {
+	private final UserDAO userDAO;
+	
+	public AuthService() {
+		this.userDAO = new UserDAO();
+	}
+	
+	public User login(String username, String password) {
+		User user = userDAO.findByUsername(username);
+		
+		if (user != null && PasswordUtil.verify(password, user.getHashedPassword())) {
+			return user;
+		}
+		return null;
+	}
+	public boolean register(User newUser) {
+		if (userDAO.existsbyUsername(newUser.getUsername())) {
+			return false;
+		}
+		
+		String hashedPassword = PasswordUtil.hash(newUser.getHashedPassword());
+		newUser.setHashedPassword(hashedPassword);
+		
+		return userDAO.save(newUser);
+	}
 }
