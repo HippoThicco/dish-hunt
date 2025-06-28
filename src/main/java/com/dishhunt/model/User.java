a957fe823a0274a108722620b109db8922fef980
HippoThicco <oscarye2126@gmail.com>
2025-06-28 18:52:08 -0400

    created User constructors and getters and setters

--- src/main/java/com/dishhunt/model/User.java
+++ src/main/java/com/dishhunt/model/User.java
@@ -1,6 +1,7 @@
 package com.dishhunt.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class User {
@@ -14,6 +15,17 @@ public class User {
 	private List<Recipe> contributions;
 	private List<Recipe> favourites;
 	private String profilePicturePath;
+	
+	public User(int id, String username, String hashedPassword, LocalDate joinDate) {
+		this.id = id;
+		this.username = username;
+		this.hashedPassword = hashedPassword;
+		this.joinDate = joinDate;
+	}
+	
+	public User(String username, String hashedPassword) {
+		this(0, username, hashedPassword, null);
+	}
 	
 	public String getHashedPassword() {
 		return hashedPassword;
@@ -29,5 +41,69 @@ public class User {
 
 	public int getId() {
 		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public LocalDate getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(LocalDate joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public List<Recipe> getContributions() {
+		return contributions;
+	}
+
+	public void setContributions(List<Recipe> contributions) {
+		this.contributions = contributions;
+	}
+
+	public List<Recipe> getFavourites() {
+		return favourites;
+	}
+
+	public void setFavourites(List<Recipe> favourites) {
+		this.favourites = favourites;
+	}
+
+	public String getProfilePicturePath() {
+		return profilePicturePath;
+	}
+
+	public void setProfilePicturePath(String profilePicturePath) {
+		this.profilePicturePath = profilePicturePath;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 }
