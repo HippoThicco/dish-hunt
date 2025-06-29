51fc8f2074d8c38181fd9ccda11f715ad68dff27
HippoThicco <oscarye2126@gmail.com>
2025-06-28 20:25:59 -0400

    added getters and setters for ingredient class

--- src/main/java/com/dishhunt/model/Ingredient.java
+++ src/main/java/com/dishhunt/model/Ingredient.java
@@ -4,4 +4,38 @@ public class Ingredient {
 	private String name;
 	private double quantity;
 	private String unit;
+	
+	public Ingredient(String name, double quantity, String unit) {
+		this.name = name;
+		this.quantity = quantity;
+		this.unit = unit;
+	}
+	
+	public Ingredient() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	
 }
