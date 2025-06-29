f277d7bf643fc64266e3623b8c2d8f22204493a5
HippoThicco <oscarye2126@gmail.com>
2025-06-28 20:26:13 -0400

    added empty constructor in dish for recipeDAO

--- src/main/java/com/dishhunt/model/Dish.java
+++ src/main/java/com/dishhunt/model/Dish.java
@@ -13,11 +13,50 @@ public class Dish {
 		this.name = dishName;
 	}
 	
+	public Dish() {
+	}
+
 	public void addRecipe(Recipe recipe) {
 		recipes.add(recipe);
 	}
 
 	public List<Recipe> getRecipes() {
 		return recipes;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCuisine() {
+		return cuisine;
+	}
+
+	public void setCuisine(String cuisine) {
+		this.cuisine = cuisine;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setRecipes(List<Recipe> recipes) {
+		this.recipes = recipes;
 	}
 }
