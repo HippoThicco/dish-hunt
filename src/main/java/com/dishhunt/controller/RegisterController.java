73776b343e9e847871ee9ea1662edc4ede37e5fd
HippoThicco <oscarye2126@gmail.com>
2025-07-04 22:47:20 -0400

    created controller for register page

---
+++ src/main/java/com/dishhunt/controller/RegisterController.java
@@ -0,0 +1,33 @@
+package com.dishhunt.controller;
+
+import com.dishhunt.service.AuthService;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class RegisterController {
+	private final AuthService authService = new AuthService();
+	
+	@FXML private TextField usernameField;
+	@FXML private PasswordField passwordField;
+	@FXML private Label messageLabel;
+	
+	@FXML
+	public void handleRegister() {
+		String username = usernameField.getText();
+		String password = passwordField.getText();
+		
+		boolean success = authService.register(username, password);
+		
+		if (success) {
+			messageLabel.setText("Registered successfully!");
+		} else {
+			messageLabel.setText("Username already exists or invalid.");
+		}
+	}
+	
+	@FXML
+	public void goToLogin() {
+		// TODO: switch scene to login.fxml
+	}
+}
