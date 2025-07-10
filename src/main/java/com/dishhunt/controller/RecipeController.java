52bc50d92609d9032913690cb52c6c90329e2818
HippoThicco <oscarye2126@gmail.com>
2025-07-09 20:16:28 -0400

    created RecipeController and placeholder methods

---
+++ src/main/java/com/dishhunt/controller/RecipeController.java
@@ -0,0 +1,14 @@
+package com.dishhunt.controller;
+
+import com.dishhunt.model.Recipe;
+
+public class RecipeController {
+	
+	public void displayRecipe(Recipe recipe) {};
+
+	public void likeRecipe() {}
+	
+	public void favouriteRecipe() {};
+	
+	public void comment(String text) {};
+}
