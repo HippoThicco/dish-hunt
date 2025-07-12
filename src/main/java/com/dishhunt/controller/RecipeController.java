4b0d072d51c34dc86de1216a6f20f5e78afcdbbf
HippoThicco <oscarye2126@gmail.com>
2025-07-11 21:44:15 -0400

    changed RecipeController's ingredients label to ListView

--- src/main/java/com/dishhunt/controller/RecipeController.java
+++ src/main/java/com/dishhunt/controller/RecipeController.java
@@ -1,5 +1,8 @@
 package com.dishhunt.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.dishhunt.model.Recipe;
 
 import javafx.fxml.FXML;
@@ -9,7 +12,7 @@ public class RecipeController {
 	@FXML private Label recipeTitle;
 	@FXML private Label recipeDescription;
 	@FXML private Label recipeCalories;
-	@FXML private Label recipeIngredients;
+	@FXML private ListView<String> recipeIngredients;
 	@FXML private TextArea recipeInstructions;
 	@FXML private TextField commentBox;
 	
@@ -17,7 +20,16 @@ public class RecipeController {
 		recipeTitle.setText(recipe.getTitle());
 		recipeDescription.setText(recipe.getDescription());
 		recipeCalories.setText("Calories: " + recipe.getCalories());
-		recipeIngredients.setText("" + recipe.getIngredients());
+		recipeIngredients.getItems().clear();
+		List<String> ingredientStrings = recipe.getIngredients().stream()
+			    .map(ingredient -> String.format("%.2f %s %s", 
+			                                     ingredient.getQuantity(), 
+			                                     ingredient.getUnit(), 
+			                                     ingredient.getName()))
+			    .collect(Collectors.toList());
+
+			recipeIngredients.getItems().setAll(ingredientStrings);
+
 		recipeInstructions.setText(recipe.getInstructions());
 	}
 
